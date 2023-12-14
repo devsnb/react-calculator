0c232085f69176ce66775d6017737e39144d4ad5
Snehangshu Biswas <dev.snehangshu@gmail.com>
2023-12-14 18:13:43 +0530

    refactor: Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -14,95 +14,32 @@ const getStyleName = name => {
 }
 
 const Button = ({ value }) => {
-	const { calc, setCalc } = useCalculatorValues()
-
-	// User click comma/period
-	const commaClick = () => {
-		setCalc({
-			...calc,
-			num: !calc.num.toString().includes('.') ? calc.num + value : calc.num
-		})
-	}
-	// User click C
-	const resetClick = () => {
-		setCalc({ sign: '', num: 0, res: 0 })
-	}
-	// User click number
-	const handleClickButton = () => {
-		const numberString = value.toString()
-
-		let numberValue
-		if (numberString === '0' && calc.num === 0) {
-			numberValue = '0'
-		} else {
-			numberValue = Number(calc.num + numberString)
-		}
-
-		setCalc({
-			...calc,
-			num: numberValue
-		})
-	}
-	// User click operation
-	const signClick = () => {
-		setCalc({
-			sign: value,
-			res: !calc.res && calc.num ? calc.num : calc.res,
-			num: 0
-		})
-	}
-	// User click equals
-	const equalsClick = () => {
-		if (calc.res && calc.num) {
-			const math = (a, b, sign) => {
-				const result = {
-					'+': (a, b) => a + b,
-					'-': (a, b) => a - b,
-					X: (a, b) => a * b,
-					'/': (a, b) => a / b
-				}
-				return result[sign](a, b)
-			}
-			setCalc({
-				res: math(calc.res, calc.num, calc.sign),
-				sign: '',
-				num: 0
-			})
-		}
-	}
-	// User click percent
-	const percentClick = () => {
-		setCalc({
-			num: calc.num / 100,
-			res: calc.res / 100,
-			sign: ''
-		})
-	}
-	// User click invert button
-	const invertClick = () => {
-		setCalc({
-			num: calc.num ? calc.num * -1 : 0,
-			res: calc.res ? calc.res * -1 : 0,
-			sign: ''
-		})
-	}
+	const {
+		decimalInput,
+		reset,
+		numberInputHandler,
+		selectOperation,
+		calculateOutput,
+		calculatePercentage,
+		toggleSign
+	} = useCalculatorValues()
 
 	const handelBtnClick = () => {
 		const results = {
-			'.': commaClick,
-			C: resetClick,
-			'/': signClick,
-			X: signClick,
-			'-': signClick,
-			'+': signClick,
-			'=': equalsClick,
-			'%': percentClick,
-			'+-': invertClick
+			'.': () => decimalInput(value),
+			C: reset,
+			'/': () => selectOperation(value),
+			X: () => selectOperation(value),
+			'-': () => selectOperation(value),
+			'+': () => selectOperation(value),
+			'=': calculateOutput,
+			'%': calculatePercentage,
+			'+-': toggleSign
 		}
 		if (results[value]) {
 			return results[value]()
 		} else {
-			return handleClickButton()
+			return numberInputHandler(value)
 		}
 	}
 
--- src/features/CalculatorContext.jsx
+++ src/features/CalculatorContext.jsx
@@ -3,8 +3,28 @@ import { createContext, useContext, useState } from 'react'
 const CalculatorContext = createContext(null)
 
 export const useCalculatorValues = () => {
-	const { calc, setCalc } = useContext(CalculatorContext)
-	return { calc, setCalc }
+	const {
+		calc,
+		setCalc,
+		decimalInput,
+		reset,
+		numberInputHandler,
+		selectOperation,
+		calculateOutput,
+		calculatePercentage,
+		toggleSign
+	} = useContext(CalculatorContext)
+	return {
+		calc,
+		setCalc,
+		decimalInput,
+		reset,
+		numberInputHandler,
+		selectOperation,
+		calculateOutput,
+		calculatePercentage,
+		toggleSign
+	}
 }
 
 export const CalculatorContextProvider = ({ children }) => {
@@ -14,8 +34,108 @@ export const CalculatorContextProvider = ({ children }) => {
 		res: 0
 	})
 
+	// when a decimal is entered
+	const decimalInput = value => {
+		setCalc(prevCalc => {
+			return {
+				...prevCalc,
+				num: !prevCalc.num.toString().includes('.')
+					? prevCalc.num + value
+					: prevCalc.num
+			}
+		})
+	}
+
+	// reset calculator
+	const reset = () => {
+		setCalc({ sign: '', num: 0, res: 0 })
+	}
+
+	// handles number input
+	const numberInputHandler = value => {
+		const numberString = value.toString()
+
+		let numberValue
+		if (numberString === '0' && calc.num === 0) {
+			numberValue = '0'
+		} else {
+			numberValue = Number(calc.num + numberString)
+		}
+
+		setCalc(prevCalc => {
+			return {
+				...prevCalc,
+				num: numberValue
+			}
+		})
+	}
+
+	// operation select handler
+	const selectOperation = value => {
+		setCalc(prevCalc => {
+			return {
+				sign: value,
+				res: !prevCalc.res && prevCalc.num ? prevCalc.num : prevCalc.res,
+				num: 0
+			}
+		})
+	}
+
+	const calculateOutput = () => {
+		if (calc.res && calc.num) {
+			const math = (a, b, sign) => {
+				const result = {
+					'+': (a, b) => a + b,
+					'-': (a, b) => a - b,
+					X: (a, b) => a * b,
+					'/': (a, b) => a / b
+				}
+				return result[sign](a, b)
+			}
+			setCalc(prevCalc => {
+				return {
+					res: math(prevCalc.res, prevCalc.num, prevCalc.sign),
+					sign: '',
+					num: 0
+				}
+			})
+		}
+	}
+
+	const calculatePercentage = () => {
+		setCalc(prevCalc => {
+			return {
+				num: prevCalc.num / 100,
+				res: prevCalc.res / 100,
+				sign: ''
+			}
+		})
+	}
+
+	const toggleSign = () => {
+		setCalc(prevCalc => {
+			return {
+				num: prevCalc.num ? prevCalc.num * -1 : 0,
+				res: prevCalc.res ? prevCalc.res * -1 : 0,
+				sign: ''
+			}
+		})
+	}
+
 	return (
-		<CalculatorContext.Provider value={{ calc, setCalc }}>
+		<CalculatorContext.Provider
+			value={{
+				calc,
+				setCalc,
+				decimalInput,
+				reset,
+				numberInputHandler,
+				selectOperation,
+				calculateOutput,
+				calculatePercentage,
+				toggleSign
+			}}
+		>
 			{children}
 		</CalculatorContext.Provider>
 	)
